2822d94e2a4426e66df1c8b1f55658d4cf19cc94
yu <pomelo20001@gmail.com>
2022-04-21 11:52:12 +0800

    testing data set
    from:
    https://archive.ics.uci.edu/ml/datasets/Student+Performance

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+data = pd.read_csv("student-mat.csv",sep=";")
+data = data[["G1", "G2", "G3"]]
+predict = "G3"
+
+x = np.array(data.drop([predict],1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train,y_train)
+acc = linear.score(x_test,y_test)
+print(acc)
