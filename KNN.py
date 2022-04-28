e0bf3a1645085deeae2f01ca3861ef4ca9a977fc
yu <90242233+Yu-20001@users.noreply.github.com>
2022-04-28 10:26:53 +0800

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,37 @@
+import pandas
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pandas.read_csv("car.data")
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+predict = "class"
+
+x = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors= 7)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["acc","good","unacc","vgood"]
+print("test size: ",len(predicted))
+for x in range(len(predicted)):
+    print("predicted_y:", names[predicted[x]],"input_x: ",x_test[x],"actual_y: ",names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 7, True)
+    print("N:", n)
